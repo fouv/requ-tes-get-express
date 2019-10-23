514bf96b31bd9b906d60b9a1c72a5340a7dafe81
Florence Bouvier <florencebres1@gmail.com>
2019-10-23 12:18:30 +0200

    requete get http

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+/* GET food page. */
+router.get('/select-food', function(req, res, next) {
+    console.log(req.query);
+
+    res.render('food', { type: req.query.type });
 });
 
 module.exports = router;
